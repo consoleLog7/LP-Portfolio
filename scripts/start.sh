439f8ef06b2c4fe1d1e2f6151dd85b6df46b6148
logan <loganpugsley7@gmail.com>
2026-06-13 15:06:43 -0300

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-sudo systemctl restart streamlit
+cd /home/ec2-user/LP-Portfolio
+
+nohup python3 -m streamlit run main.py \
+  --server.port 8501 \
+  --server.address 0.0.0.0 \
+  > streamlit.log 2>&1 &
